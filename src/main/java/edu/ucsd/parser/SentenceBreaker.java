8d6d57b5a0309d9d53d283cb4e922dd34af96f02
Roger Tanuatmadja <rtanuatm@eng.ucsd.edu>
2014-06-25 21:18:50 -0700

    Manual parsing of sentences from the Disney Financial Statement and inserting into the Neo4J Database

--- src/main/java/edu/ucsd/parser/SentenceBreaker.java
+++ src/main/java/edu/ucsd/parser/SentenceBreaker.java
@@ -5,9 +5,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SentenceBreaker {
 
+	/**
+	 * Utilizes a hack to work around problems with the BreakIterator i.e. 
+	 * 1. In the sentence Already in the first quarter of fiscal 2014, we’ve had tremendous success with Thor: The Dark World from Marvel, Frozen from Walt Disney Animation, and Saving Mr. Banks from Walt Disney Studios. 
+     *    the BreakIterator would detect a new sentence after Saving Mr. (dot notation)
+	 * 
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException {
 		InputStream is = SentenceBreaker.class.getClassLoader().getResourceAsStream("DFS.txt");
 		BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
@@ -21,14 +32,28 @@ public class SentenceBreaker {
 		
 		// String paragraph = "I'm going to eat something. But I don't know what yet.";
 		String paragraph = sb.toString();
+		List<String> sentences = new ArrayList<String>();
 		BreakIterator boundary = BreakIterator.getSentenceInstance();
 		boundary.setText(paragraph);
+		String previous = "";
 		int start = boundary.first();
 	    for (int end = boundary.next();
 	          end != BreakIterator.DONE;
 	          start = end, end = boundary.next()) {
-	    	  count++;
-	          System.out.println(paragraph.substring(start,end));
+	    	  String text = paragraph.substring(start, end);
+	    	  if (text.startsWith("Banks from Walt Disney Studios.") || text.startsWith("It will continue to grow as long as there is imagination left")) {
+	    		  previous = previous.concat(text);
+	    		  sentences.set(count - 1, previous);
+	    	  } else {
+	    		  sentences.add(text);
+	    		  count++;
+	    	  }
+	    	  
+	          previous = text;
+	    }
+	    
+	    for (String sentence : sentences) {
+	    	System.out.println(sentence);
 	    }
 	    
 	    System.out.println("Total number of sentences: " + count);
--- src/main/java/edu/ucsd/parser/StanfordParser2.java
+++ src/main/java/edu/ucsd/parser/StanfordParser2.java
@@ -1,5 +1,17 @@
 package edu.ucsd.parser;
 
+import edu.ucsd.dao.SentenceDao;
+import edu.ucsd.model.Sentence;
+import edu.ucsd.model.Word;
+import edu.ucsd.model.WordToWordDependency;
+import edu.ucsd.system.SystemApplicationContext;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.text.BreakIterator;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -7,12 +19,8 @@ import java.util.Map;
 import java.util.Properties;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.util.StopWatch;
 
-import edu.ucsd.dao.SentenceDao;
-import edu.ucsd.model.Sentence;
-import edu.ucsd.model.Word;
-import edu.ucsd.model.WordToWordDependency;
-import edu.ucsd.system.SystemApplicationContext;
 import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
@@ -31,77 +39,120 @@ import edu.stanford.nlp.trees.TypedDependency;
 import edu.stanford.nlp.util.CoreMap;
 
 public class StanfordParser2 {
-	public static void main(String[] args) {
-	    ApplicationContext appContext = SystemApplicationContext.getApplicationContext();
-	    SentenceDao sentenceDao = SentenceDao.class.cast(appContext.getBean("sentenceDao")); 
+	
+	private static List<String> readDisneyFinancialStatement() throws IOException {
+		InputStream is = SentenceBreaker.class.getClassLoader().getResourceAsStream("DFS.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+		StringBuilder sb = new StringBuilder();
+		String inputLine; 
+		while((inputLine = br.readLine()) != null) {
+			sb.append(inputLine);
+		}
 		
-		Properties props = new Properties();
-	    props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse");
-	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-	    
-	    // read some text in the text variable
-	    String text = "The strongest rain ever recorded in India shut down the financial hub of Mumbai, snapped communication lines, closed airports and forced thousands of people to sleep in their offices or walk home during the night, officials said today.";
-	    
-	    // create an empty Annotation just with the given text
-	    Annotation document = new Annotation(text);
-	    
-	    // run all Annotators on this text
-	    pipeline.annotate(document);
-	    
-	    // these are all the sentences in this document
-	    // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-	    List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-	    
-	    // Maintain a list of words that has already been seen
-	    Map<String, Word> seenWords = new HashMap<String, Word>();
-	    Word root = Word.newWord("ROOT");
-	    sentenceDao.save(root);
-	    seenWords.put(root.getText(), root);
-	    
-	    for(CoreMap sentence: sentences) {
-	    	// traversing the words in the current sentence
-	        // a CoreLabel is a CoreMap with additional token-specific methods
-	    	Sentence newSentence = Sentence.newSentence(text);
-	    	
-	        for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
-	          // this is the text of the token
-	          String word = token.get(TextAnnotation.class);
-	          // this is the POS tag of the token
-	          String pos = token.get(PartOfSpeechAnnotation.class);
-	          // this is the NER label of the token
-	          String ne = token.get(NamedEntityTagAnnotation.class);
-	          System.out.println("Word, POS, and NE: " + word + ", " + pos + ", " + ne);
-	          Word seenWord = seenWords.get(word);
-	          if(seenWord == null) {
-	        	  seenWord = Word.newWord(word);
-	        	  seenWords.put(word, seenWord);
-	          }
-	          seenWord.addLabel(pos);
-	          sentenceDao.save(seenWord);
-	          newSentence.addWord(seenWord);
-	        }
-	        
-	        System.out.println("Number of words: " + newSentence.countNumberOfWords());
-	        
-	        sentenceDao.save(newSentence);
-	        
-	    	// this is the parse tree of the current sentence
-	        Tree tree = sentence.get(TreeAnnotation.class);
-	        // Get dependency tree
-	        TreebankLanguagePack tlp = new PennTreebankLanguagePack();
-	        GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
-	        GrammaticalStructure gs = gsf.newGrammaticalStructure(tree);
-	        Collection<TypedDependency> tds = gs.typedDependenciesCollapsed();
-	        for(TypedDependency td : tds) {
-	        	
-	        	System.out.println(td);
-	        	System.out.println(td.reln().getLongName());
-	        	System.out.println(td.gov().nodeString());
-	        	System.out.println("Dependency Name: " + td.dep().nodeString() +  " Node: " + td.reln());
-	        	WordToWordDependency dependency = new WordToWordDependency(seenWords.get(td.gov().nodeString()), seenWords.get(td.dep().nodeString()), td.reln().toString());
-	        	sentenceDao.save(dependency);
-	        }
-	        System.out.println("Number of dependencies: " + tds.size());
+		int count = 0;		
+		String paragraph = sb.toString();
+		List<String> sentences = new ArrayList<String>();
+		BreakIterator boundary = BreakIterator.getSentenceInstance();
+		boundary.setText(paragraph);
+		String previous = "";
+		int start = boundary.first();
+	    for (int end = boundary.next();
+	          end != BreakIterator.DONE;
+	          start = end, end = boundary.next()) {
+	    	  String text = paragraph.substring(start, end);
+	    	  if (text.startsWith("Banks from Walt Disney Studios.") || text.startsWith("It will continue to grow as long as there is imagination left")) {
+	    		  previous = previous.concat(text.trim());
+	    		  sentences.set(count - 1, previous);
+	    	  } else {
+	    		  sentences.add(text.trim());
+	    		  count++;
+	    	  }
+	    	  
+	          previous = text;
 	    }
+
+	    return sentences;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		StopWatch stopWatch = new StopWatch("Parsing and Insertion");
+		
+		stopWatch.start();
+		ApplicationContext appContext = SystemApplicationContext.getApplicationContext();
+		SentenceDao sentenceDao = SentenceDao.class.cast(appContext.getBean("sentenceDao")); 
+
+		Properties props = new Properties();
+		props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+		List<String> disneyFinancialStatement = readDisneyFinancialStatement();
+
+		// Maintain a list of words that has already been seen
+		Map<String, Word> seenWords = new HashMap<String, Word>();
+		Word root = Word.newWord("ROOT");
+		sentenceDao.save(root);
+		seenWords.put(root.getText(), root);
+
+		for(String text : disneyFinancialStatement ) {
+			// create an empty Annotation just with the given text
+			Annotation document = new Annotation(text);
+
+			// run all Annotators on this text
+			pipeline.annotate(document);
+
+			// these are all the sentences in this document
+			// a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+			List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+
+			for(CoreMap sentence: sentences) {
+				// traversing the words in the current sentence
+				// a CoreLabel is a CoreMap with additional token-specific methods
+				Sentence newSentence = Sentence.newSentence(text);
+
+				for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+					// this is the text of the token
+					String word = token.get(TextAnnotation.class);
+					// this is the POS tag of the token
+					String pos = token.get(PartOfSpeechAnnotation.class);
+					// this is the NER label of the token
+					String ne = token.get(NamedEntityTagAnnotation.class);
+					// System.out.println("Word, POS, and NE: " + word + ", " + pos + ", " + ne);
+					Word seenWord = seenWords.get(word);
+					if(seenWord == null) {
+						seenWord = Word.newWord(word);
+						seenWords.put(word, seenWord);
+					}
+					seenWord.addLabel(pos);
+					seenWord.addLabel(ne);
+					sentenceDao.save(seenWord);
+					newSentence.addWord(seenWord);
+				}
+
+				// System.out.println("Number of words: " + newSentence.countNumberOfWords());
+
+				sentenceDao.save(newSentence);
+
+				// this is the parse tree of the current sentence
+				Tree tree = sentence.get(TreeAnnotation.class);
+				// Get dependency tree
+				TreebankLanguagePack tlp = new PennTreebankLanguagePack();
+				GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
+				GrammaticalStructure gs = gsf.newGrammaticalStructure(tree);
+				Collection<TypedDependency> tds = gs.typedDependenciesCollapsed();
+				for(TypedDependency td : tds) {
+					/*
+					System.out.println(td);
+					System.out.println(td.reln().getLongName());
+					System.out.println(td.gov().nodeString());
+					System.out.println("Dependency Name: " + td.dep().nodeString() +  " Node: " + td.reln());
+					*/
+					WordToWordDependency dependency = new WordToWordDependency(seenWords.get(td.gov().nodeString()), seenWords.get(td.dep().nodeString()), td.reln().toString());
+					sentenceDao.save(dependency);
+				}
+				// System.out.println("Number of dependencies: " + tds.size());
+			}
+		}
+		stopWatch.stop();
+		stopWatch.prettyPrint();
 	}
 }

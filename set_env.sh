57829495bacbaa86dcfc4f837dc62e4e7925ba67
Roger Tanuatmadja <rtanuatm@eng.ucsd.edu>
2014-06-30 20:46:43 -0700

    Compilation would only work with JDK 7.0 and above so set_env.sh has been updated, plus updated pom.xml for running neo4j unit tests

---
+++ set_env.sh
@@ -0,0 +1,4 @@
+export M2_HOME=/Users/rogertan/TritonFitLife/apache-maven-3.2.1
+export M2=$M2_HOME/bin
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home
+export PATH=$PATH:$M2:$JAVA_HOME
